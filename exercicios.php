b4e9fc945cec7ede0b5ed56e5b12770c197a80dd
PauloHNDS <69486078+PauloHNDS@users.noreply.github.com>
2021-12-04 18:50:45 -0300

    Configurando a visualização do exercício

---
+++ exercicios.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/sytle.css">
+    <title>Exercicio</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso de PHP</h1>
+        <h2>Visualização do Execício</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R E ALUNOS © <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
